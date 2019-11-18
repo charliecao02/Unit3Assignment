5370d55530b0e8d07901985b236dfbd53fbe1ad2
charliecao02 <55104201+charliecao02@users.noreply.github.com>
2019-11-18 13:44:49 -0800

    Add files via upload

--- Unit3Practice/PigLatin.java
+++ Unit3Practice/PigLatin.java
@@ -1,15 +1,31 @@
 class PigLatin{
-    public static void main(String str){
+    public static String main(String str){
         String newstr="";
         int len=str.length();
         int stop=0, start=0;
         for(int i=0;i<len;i++){
             if(str.charAt(i)==' ') {
                 stop=i;
-                for(int i1=start;i1<stop;i++){
-                    
+                if(Math.abs(start-stop)>1){
+                    newstr+=str.substring(start+1,stop);
+                    newstr+=str.substring(start,start+1);
+                    newstr+="ay";
                 }
+                else newstr+=str.substring(start,start+1);
+                newstr+=" ";
+                i++;
+                start=i;
+            }
+            if(i==len-1){
+                stop=i;
+                if(Math.abs(start-stop)>1){
+                    newstr+=str.substring(start+1,stop);
+                    newstr+=str.substring(start,start+1);
+                    newstr+="ay";
+                }
+                else newstr+=str.substring(start,start+1);
             }
         }
+        return newstr;
     }
 }
